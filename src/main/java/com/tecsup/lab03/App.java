29893d635f2a963eb0900a8b388221babaf16894
Alan <175237675+branch-main@users.noreply.github.com>
2025-09-01 16:31:27 -0500

    cambios sprint

--- src/main/java/com/tecsup/lab03/App.java
+++ src/main/java/com/tecsup/lab03/App.java
@@ -5,6 +5,6 @@ package com.tecsup.lab03;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola desde sprint-1");
     }
 }
